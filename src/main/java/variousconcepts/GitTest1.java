b5d292d7832758e80aef03f4dd9dea5c8571a796
Ashish Nepal <ashishnep@hotmail.com>
2020-11-13 14:17:29 -0600

    newBrach1

--- src/main/java/variousconcepts/GitTest1.java
+++ src/main/java/variousconcepts/GitTest1.java
@@ -5,7 +5,7 @@ public class GitTest1 {
 	public static void main(String[] args) {
 		System.out.println("Git test");
 		System.out.println("Git test1");
-		System.out.println("Git test2");
+		System.out.println("Git test3");
 		
 
 	}
